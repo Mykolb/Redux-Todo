06787ecbf42832933373ba37af821700b0611c3a
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-03-19 18:27:24 -0400

    creating event and change functions in todoList

--- todo/src/components/todoList.js
+++ todo/src/components/todoList.js
@@ -1,2 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+//import name of actions//
+import { addToDo, toggleItem } from '../actions';
+
+class todoList extends React.Component {
+    state = {
+        newTodo: []
+    };
+}
 
 
+
+//handleChanges = event => {}
+
+
+//addTodo = event => {event}
+
+
+//toggleItem = (event, index) => 
+
+
+render() {
+    return (
+    <div className='todo-list'>
+    <input
+        type='text'
+        value= {this.state.newTodo}
+        onChange={this.handleChanges}
+        placeholder='Add New Item'
+    />
+    </div>
+    <button>Add Item</button>
+
+    );
+}
+
+//mapStateToProps
+
+//add connect function 
